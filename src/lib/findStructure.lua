37a159aa4783c1596117f1cc6f8519b94e01aa6a
Astrid <github@astrid.exposed>
2024-02-29 01:05:37 +0100

    improve

--- src/lib/findStructure.lua
+++ src/lib/findStructure.lua
@@ -4,16 +4,21 @@ local pluralize = require( 'util.pluralize' )
 --- Look for a structure in starmap
 --- A structure can be an astronomical anomaly
 ---@param structureType string The type of structure (system/object)
----@param structureName string The name/code/designation of the structure in Star Citizen
+---@param structureIdentifier string The name/code/designation/id of the structure in Star Citizen
 ---@return nil | system | object
-return function ( structureType, structureName )
+return function ( structureType, structureIdentifier )
     local structures = data[ pluralize( structureType ) ]
     if structures == nil then return nil end -- Invalid type
 
-    structureName = mw.ustring.lower( structureName )
+    structureIdentifier = mw.ustring.lower( structureIdentifier )
 
     for _, structure in ipairs( structures ) do
-        if mw.ustring.lower( structure.name or '' ) == structureName or mw.ustring.lower( structure.designation or '' ) == structureName or mw.ustring.lower( structure.code or '' ) == structureName then
+        if
+            mw.ustring.lower( structure.name or '' ) == structureIdentifier or
+            mw.ustring.lower( structure.designation or '' ) == structureIdentifier or
+            mw.ustring.lower( structure.code ) == structureIdentifier or
+            structure.id == structureIdentifier
+        then
             return structure
         end
     end
